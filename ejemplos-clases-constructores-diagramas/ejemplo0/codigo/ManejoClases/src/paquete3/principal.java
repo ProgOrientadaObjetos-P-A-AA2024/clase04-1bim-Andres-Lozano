f31bf58f2e0031c1a9af48ded218d4a523b203a3
Andres-Lozano <wilsonandres135@gmail.com>
2024-05-01 16:34:24 -0500

    actualizacion

---
+++ ejemplos-clases-constructores-diagramas/ejemplo0/codigo/ManejoClases/src/paquete3/principal.java
@@ -0,0 +1,15 @@
+
+package paquete3;
+
+/**
+ *
+ * @author Usuario iTC
+ */
+public class principal {
+     public static void main (String []arg){
+         // en esta clse se ha creado un objeto con un constructor por defecto 
+         //por lo que no puede recibir especificaciones en especifico
+         Computadora1 computadoraTrabajo = new Computadora1();
+     }
+    
+}
